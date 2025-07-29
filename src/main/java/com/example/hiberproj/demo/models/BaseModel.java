16176e0ae4cf03892acf60817a94735e8ec1a9df
recursion <skn054@gmail.com>
2025-07-29 16:59:43 +0530

    initial commit

---
+++ src/main/java/com/example/hiberproj/demo/models/BaseModel.java
@@ -0,0 +1,33 @@
+package com.example.hiberproj.demo.models;
+
+import java.util.Date;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@MappedSuperclass
+public class BaseModel {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long Id;
+
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(updatable = false)
+    private Date createdAt;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    private Date updatedAt;
+
+
+}
---
+++ src/main/java/com/example/hiberproj/demo/models/Department.java
@@ -0,0 +1,10 @@
+package com.example.hiberproj.demo.models;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Department extends BaseModel{
+
+    private String name;
+
+}

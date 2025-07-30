52195aac37e7706de57faa9be86447aa157c76f6
mediakhil13 <83664745+mediakhil13@users.noreply.github.com>
2025-07-30 21:41:47 +0530

    custom fetch

--- src/main/java/com/example/hiberproj/demo/repository/StudentRepository.java
+++ src/main/java/com/example/hiberproj/demo/repository/StudentRepository.java
@@ -1,7 +1,24 @@
 package com.example.hiberproj.demo.repository;
 
+import com.example.hiberproj.demo.models.Course;
 import com.example.hiberproj.demo.models.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface StudentRepository extends JpaRepository<Student,Long> {
+
+//    @Query("select c from courses_students cs join fetch courses c on c.id = cs.courses_id join fetch student s on cs.students_id = s.id")
+//    List<Student> findAllCourseEnrolledByStudents();
+
+
+    // find all students enrolled in a course
+    @Query("select s from Student s join s.courses ")
+    List<Student> findStudentsEnrolledInAtleastACourse();
+
+    @Query("select s from Student s join fetch s.courses ")
+    List<Student> findStudentsEnrolledInAtleastACourseAndGetCourses();
 }
